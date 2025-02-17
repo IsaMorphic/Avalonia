2fb061f7a29d2b1d49faf2448f55836ae5f1bd66
Isabelle Santin <isabelle@chosenfewsoftware.com>
2025-02-17 16:05:39 -0600

    Fixed bugs in determining whether controls are offscreen

--- src/Avalonia.Controls/Presenters/ContentPresenter.cs
+++ src/Avalonia.Controls/Presenters/ContentPresenter.cs
@@ -461,6 +461,7 @@ namespace Avalonia.Controls.Presenters
 
                 if (oldChild != null)
                 {
+                    oldChild.IsVisible = false;
                     VisualChildren.Remove(oldChild);
                     logicalChildren.Remove(oldChild);
                     ((ISetInheritanceParent)oldChild).SetParent(oldChild.Parent);
@@ -492,6 +493,7 @@ namespace Avalonia.Controls.Presenters
                     logicalChildren.Add(newChild);
                 }
 
+                newChild.IsVisible = true;
                 VisualChildren.Add(newChild);
             }
 
